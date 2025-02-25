77fd15ecaabac230a7b71de35b12d2d658831574
rebecca-sacarescu <161501699+rebecca-sacarescu@users.noreply.github.com>
2025-02-25 12:25:28 +0200

    Create Product.java

---
+++ Lab1/Product.java
@@ -0,0 +1,38 @@
+package com.rebecca;
+
+abstract class Product {
+    private String name;
+    public double price;
+    private String category;
+
+    public Product(String name, double price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public abstract void sale(double percentage);
+
+    public abstract void sale(double sum, boolean fixed);
+
+
+}
